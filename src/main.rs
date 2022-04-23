d47bc2a9f4bf3c1cab07c2171e7e62e0787026b9
Hugo Kapp <hugo.kapp6@hotmail.fr>
2022-04-23 20:10:34 +0200

    Print a row of tiles

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use rand::Rng;
 use std::convert::From;
 
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 enum Passage {
     Open,
     Closed
@@ -16,25 +17,17 @@ struct Tile {
 fn main() {
     let mut rng = rand::thread_rng();
 
-    let t = rand_tile(&mut rng);
+    let tiles: Vec<Tile> =
+        (0..5).into_iter()
+            .map(|_| rand_tile(&mut rng))
+            .collect();
 
-    print_tile(&t);
+    print_tiles(&tiles);
 }
 
-/**** print_tile() ****/
+/**** print_tiles() ****/
 
-fn print_tile(tile: &Tile) {
-    println!("+{}+", format_horizontal_passage(&tile.up));
-    println!("{}   {}",
-        format_vertical_passage(&tile.left),
-        format_vertical_passage(&tile.right));
-    println!("{}   {}",
-        format_vertical_passage(&tile.left),
-        format_vertical_passage(&tile.right));
-    println!("+{}+", format_horizontal_passage(&tile.down));
-}
-
-fn format_horizontal_passage(ps: &Passage) -> &str {
+fn format_horizontal_passage(ps: Passage) -> &'static str {
     use Passage::*;
     match ps {
         Open   => "   ",
@@ -42,12 +35,90 @@ fn format_horizontal_passage(ps: &Passage) -> &str {
     }
 }
 
-fn format_vertical_passage(ps: &Passage) -> char {
+fn format_vertical_passage(ps: Passage) -> &'static str {
     use Passage::*;
     match ps {
-        Open   => ' ',
-        Closed => '|',
+        Open   => " ",
+        Closed => "|",
     }
+}
+
+struct RowDisplay {
+    top_line: String,
+    mid_line: String,
+    bot_line: String,
+    mid_free: bool
+}
+
+fn print_tiles(tiles: &[Tile]) {
+    let mut display = RowDisplay::new();
+
+    for t in tiles {
+        display.push_tile(&t);
+    }
+
+    display.print();
+}
+
+impl RowDisplay {
+    fn new() -> Self {
+        RowDisplay {
+            top_line: String::new(),
+            mid_line: String::new(),
+            bot_line: String::new(),
+            mid_free: true,
+        }
+    }
+
+    fn push_tile(&mut self, t: &Tile) {
+        self.push_top(t.up);
+        self.push_mid(t.left, t.right);
+        self.push_bot(t.down);
+    }
+
+    fn push_top(&mut self, ps: Passage) {
+        append_horizontal_fmt(&mut self.top_line, ps);
+    }
+
+    fn push_bot(&mut self, ps: Passage) {
+        append_horizontal_fmt(&mut self.bot_line, ps);
+    }
+
+    #[allow(unused_parens)]
+    fn push_mid(&mut self, left: Passage, right: Passage) {
+        if self.mid_free {
+            append_const(&mut self.mid_line, format_vertical_passage(left));
+        }
+        append_const(&mut self.mid_line, "   ");
+
+        if right == Passage::Closed {
+            append_const(&mut self.mid_line, format_vertical_passage(right));
+        }
+        self.mid_free = (right == Passage::Open);
+    }
+
+    fn print(&self) {
+        println!("{}", self.top_line);
+        println!("{}", self.mid_line);
+        println!("{}", self.mid_line);
+        println!("{}", self.bot_line);
+    }
+}
+
+fn append_horizontal_fmt(buf: &mut String, ps: Passage) {
+    if buf.is_empty() {
+        append_corner(buf);
+    }
+    append_const(buf, format_horizontal_passage(ps));
+    append_corner(buf);
+}
+
+fn append_const(buf: &mut String, suffix: &str) {
+    buf.push_str(suffix);
+}
+
+fn append_corner(buf: &mut String) {
+    append_const(buf, "+");
 }
 
 /**** rand_tile() ****/
