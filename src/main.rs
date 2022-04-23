7b7e71f2bb8cdcdc5a372adfbfa158d7ffe17947
Hugo Kapp <hugo.kapp6@hotmail.fr>
2022-04-23 19:22:16 +0200

    Generate tiles randomly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use rand::Rng;
+use std::convert::From;
+
 enum Passage {
     Open,
     Closed
@@ -11,15 +14,14 @@ struct Tile {
 }
 
 fn main() {
-    let t = Tile {
-        left:  Passage::Open,
-        right: Passage::Closed,
-        up:    Passage::Open,
-        down:  Passage::Closed
-    };
+    let mut rng = rand::thread_rng();
+
+    let t = rand_tile(&mut rng);
 
     print_tile(&t);
 }
+
+/**** print_tile() ****/
 
 fn print_tile(tile: &Tile) {
     println!("+{}+", format_horizontal_passage(&tile.up));
@@ -45,5 +47,25 @@ fn format_vertical_passage(ps: &Passage) -> char {
     match ps {
         Open   => ' ',
         Closed => '|',
+    }
+}
+
+/**** rand_tile() ****/
+
+fn rand_tile<R: Rng>(rng: &mut R) -> Tile {
+    Tile {
+        left:  rng.gen::<bool>().into(),
+        right: rng.gen::<bool>().into(),
+        up:    rng.gen::<bool>().into(),
+        down:  rng.gen::<bool>().into(),
+    }
+}
+
+impl From<bool> for Passage {
+    fn from(b: bool) -> Self {
+        match b {
+            true  => Passage::Open,
+            false => Passage::Closed,
+        }
     }
 }
