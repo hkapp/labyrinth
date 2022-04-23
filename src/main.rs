9e09070a6ab4c97345a8aaecdca7b35ada805821
Hugo Kapp <hugo.kapp6@hotmail.fr>
2022-04-23 19:00:24 +0200

    Define the basic tile and print it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+enum Passage {
+    Open,
+    Closed
+}
+
+struct Tile {
+    left:  Passage,
+    right: Passage,
+    up:    Passage,
+    down:  Passage
+}
+
 fn main() {
-    println!("Hello, world!");
+    let t = Tile {
+        left:  Passage::Open,
+        right: Passage::Closed,
+        up:    Passage::Open,
+        down:  Passage::Closed
+    };
+
+    print_tile(&t);
+}
+
+fn print_tile(tile: &Tile) {
+    println!("+{}+", format_horizontal_passage(&tile.up));
+    println!("{}   {}",
+        format_vertical_passage(&tile.left),
+        format_vertical_passage(&tile.right));
+    println!("{}   {}",
+        format_vertical_passage(&tile.left),
+        format_vertical_passage(&tile.right));
+    println!("+{}+", format_horizontal_passage(&tile.down));
+}
+
+fn format_horizontal_passage(ps: &Passage) -> &str {
+    use Passage::*;
+    match ps {
+        Open   => "   ",
+        Closed => "---",
+    }
+}
+
+fn format_vertical_passage(ps: &Passage) -> char {
+    use Passage::*;
+    match ps {
+        Open   => ' ',
+        Closed => '|',
+    }
 }
